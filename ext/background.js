2d0b75d8b6fabbaa49e8be40b1932f74b632d9db
onepointfive <122128996+onepointfive-REAL@users.noreply.github.com>
2025-02-04 17:38:25 -0600

    Delete ext directory

--- ext/background.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => {
-    console.log("Extension installed and background script running. also why tf are you looking at logs you boomer");
-});
-
-chrome.action.onClicked.addListener((tab) => {
-    chrome.tabs.sendMessage(tab.id, { action: "toggleWebcam" });
-});
--- ext/forkbomb/window.js
+++
@@ -1,4 +0,0 @@
-function spamcurrent() {
-window.open(self, "_blank", 'menubar=yes, status=yes, toolbar=yes, resizable=yes, width=1, height=1, titlebar=yes, alwaysRaised=yes');
-}
-setInterval(spamcurrent, 1000)
